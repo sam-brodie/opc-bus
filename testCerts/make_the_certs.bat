99a84777cdd2c45ee169ffa1fe114de2e596e54d
Sam Brodie <63566766+sam-brodie@users.noreply.github.com>
2024-12-02 11:16:13 +0100

    fixed certificate generation. user must click and install the CA certificate

--- testCerts/make_the_certs.bat
+++ testCerts/make_the_certs.bat
@@ -22,7 +22,7 @@ rem rem -addext "subjectAltName = urn:open62541.client.application"
 
 
 rem make a root CA
-"C:\Program Files\OpenSSL-Win64\bin\openssl.exe" req -x509 -newkey rsa:4096 -keyout ca/ca.key -out sam_CA_root_3.crt -sha256 -days 3650 -nodes -config ca.conf
+"C:\Program Files\OpenSSL-Win64\bin\openssl.exe" req -x509 -newkey rsa:2048 -keyout ca/ca.key -out sam_CA_root_3.crt -sha256 -days 7 -nodes -config ca.conf -subj "/C=de/ST=bayern/O=tum/OU=Brodie/CN=rootCA"
 
 
 rem Make a CRL
@@ -32,14 +32,14 @@ rem Make a CRL
 
 rem Make a request to sign an OPC UA server cert
 "C:\Program Files\OpenSSL-Win64\bin\openssl.exe" genpkey -algorithm RSA -out sam_server_private_key.pem
-"C:\Program Files\OpenSSL-Win64\bin\openssl.exe" req -new -key sam_server_private_key.pem -out server.csr -config ca.conf
-"C:\Program Files\OpenSSL-Win64\bin\openssl.exe"  x509 -req -in server.csr -CA sam_CA_root_3.crt -CAkey  ca/ca.key -CAcreateserial -out samServer_3_3.crt -days 365 -extensions v3_server -extfile ca.conf
+"C:\Program Files\OpenSSL-Win64\bin\openssl.exe" req -new -key sam_server_private_key.pem -out server.csr -config ca.conf -subj "/C=de/ST=bayern/O=tum/OU=Brodie/CN=opcServer" 
+"C:\Program Files\OpenSSL-Win64\bin\openssl.exe"  x509 -req -in server.csr -CA sam_CA_root_3.crt -CAkey  ca/ca.key -CAcreateserial -out samServer_3_3.crt -days 7 -extensions v3_server -extfile ca.conf
 "C:\Program Files\OpenSSL-Win64\bin\openssl.exe" x509 -in samServer_3_3.crt -text -noout
 
 rem Make a request to sign an OPC UA client cert
 "C:\Program Files\OpenSSL-Win64\bin\openssl.exe" genpkey -algorithm RSA -out sam_client_private_key.pem
-"C:\Program Files\OpenSSL-Win64\bin\openssl.exe" req -new -key sam_client_private_key.pem -out client.csr -config ca.conf
-"C:\Program Files\OpenSSL-Win64\bin\openssl.exe"  x509 -req -in client.csr -CA sam_CA_root_3.crt -CAkey  ca/ca.key -CAcreateserial -out samClient_3_3.crt -days 365 -extensions v3_client -extfile ca.conf
+"C:\Program Files\OpenSSL-Win64\bin\openssl.exe" req -new -key sam_client_private_key.pem -out client.csr -config ca.conf -subj "/C=de/ST=bayern/O=tum/OU=Brodie/CN=opcClient" 
+"C:\Program Files\OpenSSL-Win64\bin\openssl.exe"  x509 -req -in client.csr -CA sam_CA_root_3.crt -CAkey  ca/ca.key -CAcreateserial -out samClient_3_3.crt -days 7 -extensions v3_client -extfile ca.conf
 "C:\Program Files\OpenSSL-Win64\bin\openssl.exe" x509 -in samClient_3_3.crt -text -noout
 
 rem copy /y ca/ca.crt 
